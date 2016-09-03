262430f1ed3afcd41ad9d9eaba5a5e40e3d1d3d9
Stefan Pokorny <stefan.pokorny@eacg.de>
2016-09-03 17:04:02 -0400

    added rabbitmq (yum...)

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@ source_url 'https://github.com/rabbitmq/chef-cookbook'
 
 depends 'erlang'
 depends 'yum-epel'
-#depends 'yum-erlang_solutions'
+depends 'yum-erlang_solutions'
 depends 'dpkg_autostart'
 
 supports 'debian', '>= 8.0'
